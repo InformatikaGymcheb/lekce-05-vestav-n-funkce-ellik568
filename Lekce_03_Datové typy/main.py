0599e746b97e00ed529c583b3580d3c3282f5744
ellik568 <elsudim@gymcheb.cz>
2026-03-26 12:16:18 +0100

    Update main.py

--- Lekce_03_Datové typy/main.py
+++ Lekce_03_Datové typy/main.py
@@ -1,18 +1,30 @@
 # ÚLOHA 1: Sčítací past (Cíl: 5 + 3 = 8)
 # ---------------------------------------------------------
-
+a = int(input("5"))
+b = int(input("3"))
+print("Součet je:", a + b)
 
 # ÚLOHA 2: Opakovač jména (Cíl: "Honza" a 3 -> "HonzaHonzaHonza")
 # ---------------------------------------------------------
-
+jmeno = input("Elli")
+pocet = int(input("22"))
+print(jmeno * pocet)
 
 # ÚLOHA 3: Rok narození (Cíl: 2026 - věk)
 # ---------------------------------------------------------
-
+vek = int(input("17"))
+rok_narozeni = 2026 - vek
+print("Narodil ses přibližně v roce", rok_narozeni)
 
 # ÚLOHA 4: Výplata (Cíl: mzda * hodiny)
 # ---------------------------------------------------------
-
+mzda = int(input("150"))
+hodiny = int(input("8 "))
+vyplata = mzda * hodiny
+print("Tvoje výplata je:", vyplata)
 
 # ÚLOHA 5: Magická matematika (Cíl: (x + 10) * 2)
 # ---------------------------------------------------------
+cislo = int(input("5 "))
+vysledek = (cislo + 10) * 2
+print("Výsledek je:", vysledek)
