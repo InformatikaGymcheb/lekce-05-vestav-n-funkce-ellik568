4fe00c1918a15ce29bb68a156690179c74337d72
ellik568 <elsudim@gymcheb.cz>
2026-03-26 12:10:08 +0100

    Update main.py

--- Lekce_02_Input/main.py
+++ Lekce_02_Input/main.py
@@ -2,15 +2,23 @@
 # ÚLOHA 1: Registrace (jméno a město)
 # ==========================================
 # Sem napiš svůj kód:
+jmeno = input("Elli")
+barva = input("růžová")
 
+print(f"Ahoj {jmeno}, tvoje oblíbená barva je {barva}. To je skvělá volba!")
 
 # ==========================================
 # ÚLOHA 2: Jednoduchá kalkulačka (součet)
 # ==========================================
 # Sem napiš svůj kód:
-
+cislo1 = int(input("8"))
+cislo2 = int(input("2"))
 
 # ==========================================
 # ÚLOHA 3: Výpočet psího věku (věk * 7)
 # ==========================================
 # Sem napiš svůj kód:
+print(f"Součet je: {cislo1 + cislo2}")
+vek = int(input("17"))
+
+print(f"V psích letech by ti bylo: {vek * 7} let. Haf!")
